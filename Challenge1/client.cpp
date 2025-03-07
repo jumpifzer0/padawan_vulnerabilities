a197c3b08b971abbbd9c5c8b3fd784a3b55816c9
Yi <tohyizhi9@gmail.com>
2025-03-06 18:17:42 -0800

    refactor client and server

--- Challenge1/client.cpp
+++ Challenge1/client.cpp
@@ -11,79 +11,98 @@
 using namespace std;
 
 bool debug = false;
-void sendData(SOCKET clientSocket){
-    char inputBuffer[BUF_SIZE];
-    char* formatMsgBuffer;
-    while(true){
 
-        if (fgets(inputBuffer, sizeof(inputBuffer), stdin) != nullptr) {
-            // Remove the trailing newline character (if any)
-            inputBuffer[strcspn(inputBuffer, "\n")] = '\0';
+
+
+class Client{
+    public:
+        void initialiseSocket(){
+            // initialise socket
+            if (!InitWinsock()) {
+                printf("cannot initialise socket\n");
+                return ;
+            }   
+        } ;
+
+        void connectToServer(SOCKET clientSocket){
+            if (!ConnectToServer(&clientSocket, SERVER_IP, SERVER_PORT)) {
+                printf("cannot connect to server!\n");
+                CleanupWinsock();
+                return;
+            }   
+        };
+
+        void cleanupSocket(SOCKET clientSocket){
+            // Close the socket
+            CloseSocket(clientSocket);
+
+            // Cleanup Winsock
+            CleanupWinsock();
+        };
+
+    void sendData(SOCKET clientSocket){
+        char inputBuffer[BUF_SIZE];
+        char* formatMsgBuffer;
+        while(true){
+    
+            if (fgets(inputBuffer, sizeof(inputBuffer), stdin) != nullptr) {
+                // Remove the trailing newline character (if any)
+                inputBuffer[strcspn(inputBuffer, "\n")] = '\0';
+            }
+    
+            // Send data to the server
+            formatMsgBuffer = formatMsg(inputBuffer);
+            if (SendData(clientSocket, formatMsgBuffer, strlen(formatMsgBuffer)) == 0) {
+                printf("Failed to send data\n");
+            }
+            formatMsgBuffer = NULL;
+            delete formatMsgBuffer;
         }
-
-        // Send data to the server
-        formatMsgBuffer = formatMsg(inputBuffer);
-        if (SendData(clientSocket, formatMsgBuffer, strlen(formatMsgBuffer)) == 0) {
-            printf("Failed to send data\n");
+    
+    };
+    
+    void recvData(SOCKET clientSocket){
+        char recvBuffer[BUF_SIZE_W_PADDING];
+        char* parseMsgBuffer;
+        while (true){
+            // Receive data from the server
+            int bytesReceived = ReceiveData(clientSocket, recvBuffer, sizeof(recvBuffer) - 1);
+            if (debug) printf("client received message! %d\n",bytesReceived);
+            if (bytesReceived > 0) {
+    
+                recvBuffer[bytesReceived] = '\0';  // Null-terminate the string
+                if (debug) printf("Message received in recvBuffer! %s\n",recvBuffer);
+                parseMsgBuffer = parseMsg(recvBuffer);
+                
+                printf("Received from server: %s\n", parseMsgBuffer);
+                delete parseMsgBuffer;
+                parseMsgBuffer = NULL;
+            } else if (bytesReceived < 0) {
+                printf("received failed!\n Exitting......");
+                exit(0);
+            } else {
+                if (debug) printf("received 0 bytes!\n");
+            }
         }
-        formatMsgBuffer = NULL;
-        delete formatMsgBuffer;
-    }
+    };
+};
 
-}
-
-void recvData(SOCKET clientSocket){
-    char recvBuffer[BUF_SIZE_W_PADDING];
-    char* parseMsgBuffer;
-    while (true){
-        // Receive data from the server
-        int bytesReceived = ReceiveData(clientSocket, recvBuffer, sizeof(recvBuffer) - 1);
-        if (debug) printf("client received message! %d\n",bytesReceived);
-        if (bytesReceived > 0) {
-
-            recvBuffer[bytesReceived] = '\0';  // Null-terminate the string
-            if (debug) printf("Message received in recvBuffer! %s\n",recvBuffer);
-            parseMsgBuffer = parseMsg(recvBuffer);
-            
-            printf("Received from server: %s\n", parseMsgBuffer);
-            delete parseMsgBuffer;
-            parseMsgBuffer = NULL;
-        } else if (bytesReceived < 0) {
-            printf("received failed!\n Exitting......");
-            exit(0);
-        } else {
-            if (debug) printf("received 0 bytes!\n");
-        }
-    }
-}
 
 
 int main(){
     SOCKET clientSocket;
-    char sendMessage[] = "Hello, Server!";
-    char recvBuffer[BUF_SIZE];
-    // initialise socket
-    if (!InitWinsock()) {
-        return 1;
-    }    
-
     clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    Client* client = new Client();
+    client->initialiseSocket();
     // connect to server
-    if (!ConnectToServer(&clientSocket, SERVER_IP, SERVER_PORT)) {
-        CleanupWinsock();
-        return 1;
-    }    
-
-    thread t1(recvData,clientSocket);
-    sendData(clientSocket);
+    client->connectToServer(clientSocket);
+    thread t1([client,clientSocket]{
+        client->recvData(clientSocket);
+    });
+    client->sendData(clientSocket);
     t1.join();
-    
-    // Close the socket
-    CloseSocket(clientSocket);
-
-    // Cleanup Winsock
-    CleanupWinsock();
-
+    client->cleanupSocket(clientSocket);
+    delete client;
     return 0;
 
 }
--- Challenge1/server.cpp
+++ Challenge1/server.cpp
@@ -21,162 +21,178 @@ using namespace std;
 
 bool debug = false;
 set<SOCKET> clients; 
-mutex clients_mutex; 
+mutex clientsMutex; 
 
 typedef struct {
     SOCKET sock;
     char* message;
-} message_packet;
+} messagePacket;
 
 class MessageQueue {
 public:
-    queue<message_packet> message_queue;
-    mutex message_mutex;
-    condition_variable message_cv;
+    queue<messagePacket> messageQueue;
+    mutex messageMutex;
+    condition_variable messageCV;
 
 
-    void enqueue(message_packet& message){
+    void enqueue(messagePacket& message){
         if (debug) printf("enqueue message!\n");
-        unique_lock<mutex> lock(message_mutex);
-        message_queue.push(message);
+        unique_lock<mutex> lock(messageMutex);
+        messageQueue.push(message);
         lock.unlock();
-        message_cv.notify_one();
+        messageCV.notify_one();
     };
     
-    message_packet dequeue(){
-        unique_lock<mutex> lock(message_mutex);
-        message_cv.wait(lock,[this]{return !message_queue.empty();});
-        message_packet msg = message_queue.front();
-        message_queue.pop();
+    messagePacket dequeue(){
+        unique_lock<mutex> lock(messageMutex);
+        messageCV.wait(lock,[this]{return !messageQueue.empty();});
+        messagePacket msg = messageQueue.front();
+        messageQueue.pop();
         if (debug) printf("dequeued message\n");
         return msg;
     };
 };
 
-void handle_client(SOCKET client_socket, MessageQueue *mq){
-    if (debug) printf("client thread created!\n");
-    char recvBuffer[BUF_SIZE_W_PADDING];
-    int recv_size;
-    if (debug) printf("going to lock the set for client input!\n");
-    unique_lock<mutex> lock(clients_mutex); 
-    if (debug) printf("going to insert client socket into set!\n");
-    clients.insert(client_socket);
-    lock.unlock();
-    if (debug) printf("client socket placed into set!\n");
-    //format message
-    message_packet mp;
-    mp.sock=client_socket;
-    char *c;
-    while (true){
-        if (debug) printf("trying to receive data from cient!\n");
-        int recv_size = ReceiveData(client_socket, recvBuffer, sizeof(recvBuffer) - 1);
-        if (recv_size > 0) {
-            recvBuffer[recv_size] = '\0';  // Null-terminate the string
-            
-            printf("Received from client: %s\n", recvBuffer);
-        }
-        if (recv_size == 0){
-            unique_lock<mutex> lock(clients_mutex); 
-            clients.erase(client_socket); 
-            lock.unlock();
+class Server{
+    set<SOCKET> clients; 
+    mutex clientsMutex; 
+    SOCKET serverSocket, clientSocket;
+    MessageQueue* mq;
 
-        } else if (recv_size == SOCKET_ERROR){
-            // remove client from set if client is not connected anymore
-            unique_lock<mutex> lock(clients_mutex); 
-            clients.erase(client_socket); 
-            lock.unlock();
-        } else{
-            // check if it is in the correct format:
-            if (!validateMsg(recvBuffer)){
-                continue;
+    public:
+    Server(MessageQueue* mq){this->mq = mq;};
+    void handleClient(SOCKET clientSocket, MessageQueue *mq){
+        if (debug) printf("client thread created!\n");
+        char recvBuffer[BUF_SIZE_W_PADDING];
+        int recv_size;
+        if (debug) printf("going to lock the set for client input!\n");
+        unique_lock<mutex> lock(clientsMutex); 
+        if (debug) printf("going to insert client socket into set!\n");
+        clients.insert(clientSocket);
+        lock.unlock();
+        if (debug) printf("client socket placed into set!\n");
+        //format message
+        messagePacket mp;
+        mp.sock=clientSocket;
+        char *c;
+        while (true){
+            if (debug) printf("trying to receive data from cient!\n");
+            int recv_size = ReceiveData(clientSocket, recvBuffer, sizeof(recvBuffer) - 1);
+            if (recv_size > 0) {
+                recvBuffer[recv_size] = '\0';  // Null-terminate the string
+                
+                printf("Received from client: %s\n", recvBuffer);
             }
-            c = new char[BUF_SIZE_W_PADDING];
-            // copy data from buffer to here before putting into queue
-            strncpy(c,recvBuffer,BUF_SIZE_W_PADDING);
-            mp.message = c;
-            mq->enqueue(mp); 
+            if (recv_size == 0){
+                unique_lock<mutex> lock(clientsMutex); 
+                clients.erase(clientSocket); 
+                lock.unlock();
+    
+            } else if (recv_size == SOCKET_ERROR){
+                unique_lock<mutex> lock(clientsMutex); 
+                clients.erase(clientSocket); 
+                lock.unlock();
+            } else{
+                if (!validateMsg(recvBuffer)){
+                    continue;
+                }
+                c = new char[BUF_SIZE_W_PADDING];
+                // copy data from buffer to here before putting into queue
+                strncpy(c,recvBuffer,BUF_SIZE_W_PADDING);
+                mp.message = c;
+                mq->enqueue(mp); 
+            }
+    
         }
-
+    
     }
-
-}
-
-void handle_messages(MessageQueue* mq){
-    if (debug) printf("handling messages thread created!\n");
-    message_packet data;
-    set<SOCKET>::iterator it;
-    while (true){
-        // consume message from the queue
-        data = mq->dequeue();
-        if (debug) printf("This is what is received in data! %s\n",data.message);
-        unique_lock<mutex> lock(clients_mutex);
-        for (it= clients.begin(); it != clients.end(); ++it) {
-            if (*it != data.sock){
-                if (SendData(*it, data.message, strlen(data.message)) == 0) {
-                    printf("Failed to send data to client\n");
-                    clients.erase(*it);
+    
+    void broadcastMessage(MessageQueue* mq){
+        if (debug) printf("handling messages thread created!\n");
+        messagePacket data;
+        set<SOCKET>::iterator it;
+        while (true){
+            // consume message from the queue
+            data = mq->dequeue();
+            if (debug) printf("This is what is received in data! %s\n",data.message);
+            unique_lock<mutex> lock(clientsMutex);
+            for (it= clients.begin(); it != clients.end(); ++it) {
+                if (*it != data.sock){
+                    if (SendData(*it, data.message, strlen(data.message)) == 0) {
+                        printf("Failed to send data to client\n");
+                        clients.erase(*it);
+                    }
                 }
             }
+            lock.unlock();
+            delete data.message;
         }
-        lock.unlock();
-        delete data.message;
     }
-}
 
+    void initialiseSocket(){
+        // Initialize Winsock
+        if (!InitWinsock()) {
+            return;
+        }
+
+        // Create a socket
+        serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (serverSocket == INVALID_SOCKET) {
+            printf("Socket creation failed with error: %d\n", WSAGetLastError());
+            CleanupWinsock();
+            return;
+        }
+
+        // Bind the socket to the address and port
+        if (!BindSocket(&serverSocket, SERVER_IP, SERVER_PORT)) {
+            CleanupWinsock();
+            return;
+        }
+
+        // Listen for incoming connections
+        if (!ListenForConnections(&serverSocket, BACKLOG)) {
+            CleanupWinsock();
+            return;
+        }
+
+    }
+
+    void acceptClientConnections(){
+        while (true){
+            // Accept a connection from a client
+            clientSocket = AcceptConnection(&serverSocket);
+            if (clientSocket == INVALID_SOCKET) {
+                continue;
+            }
+            thread t1(&Server::handleClient,this,clientSocket,ref(mq));
+            t1.detach(); 
+        }
+    }
+    ~Server(){
+        // Close the client socket
+        CloseSocket(clientSocket);
+
+        // Cleanup Winsock
+        CleanupWinsock();
+    };
+};
 
 int main() {
 
     MessageQueue *mq = new MessageQueue();
-    SOCKET serverSocket, clientSocket;
-    char recvBuffer[BUF_SIZE];
-    const char* sendMessage = "Hello, Client!";
-
-    // Initialize Winsock
-    if (!InitWinsock()) {
-        return 1;
-    }
-
-    // Create a socket
-    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (serverSocket == INVALID_SOCKET) {
-        printf("Socket creation failed with error: %d\n", WSAGetLastError());
-        CleanupWinsock();
-        return 1;
-    }
-
-    // Bind the socket to the address and port //TODO:check this
-    if (!BindSocket(&serverSocket, SERVER_IP, SERVER_PORT)) {
-        CleanupWinsock();
-        return 1;
-    }
-
-    // Listen for incoming connections
-    if (!ListenForConnections(&serverSocket, BACKLOG)) {
-        CleanupWinsock();
-        return 1;
-    }
-
+    Server* server = new Server(mq);
+   
+    server->initialiseSocket();
     printf("Server is listening on %s:%d...\n", SERVER_IP, SERVER_PORT);
-
-    thread t0(handle_messages,mq);
-    while (true){
-        // Accept a connection from a client
-        clientSocket = AcceptConnection(&serverSocket);
-        if (clientSocket == INVALID_SOCKET) {
-            continue;
-        }
-        thread t1(handle_client,clientSocket,mq);
-        t1.detach();
-    }
+    thread t0([server,mq]{
+        server->broadcastMessage(mq);
+    });
+    server->acceptClientConnections();
     t0.join();
 
-    // Close the client socket
-    CloseSocket(clientSocket);
-
-    // Cleanup Winsock
-    CleanupWinsock();
+    
 
     delete mq;
-
+    delete server;
     return 0;
 }
